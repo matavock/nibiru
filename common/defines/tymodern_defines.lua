98d3a43c912e5444508d324db228f4a8c212472b
matavock <fifermas@gmail.com>
2022-12-03 18:58:48 +0300

    Initial commit

---
+++ common/defines/tymodern_defines.lua
@@ -0,0 +1,13 @@
+NDefines.NGame.END_DATE = "2050.5.23.12"
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 25
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 25
+NDefines.NMilitary.ARMY_LEADER_XP_GAIN_PER_UNIT_IN_COMBAT = 0.16
+NDefines.NCountry.SPECIAL_FORCES_CAP_BASE = 0.10
+NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 48
+NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 40.0
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000;
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000;
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000;
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 10 	--Base cost to unlock a regiment slot,
+NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 4	--Base cost to change a regiment column.
+NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 8 	--Base cost to unlock a support slot
---
+++ descriptor.mod
@@ -0,0 +1,14 @@
+version="1.0"
+tags={
+	"Alternative History"
+	"Map"
+	"National Focuses"
+	"Events"
+	"Gameplay"
+}
+name="Nibiru Collapse"
+replace_path="history/states"
+replace_path="common/national_focus"
+replace_path="map/strategicregions"
+supported_version="1.12.*"
+remote_file_id="2896897889"
